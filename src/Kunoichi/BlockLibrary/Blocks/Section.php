a75ed66c1f684ae7eddc32b38d6d8d1c6a8d8b06
fumikito <guy@hametuha.com>
2020-09-11 23:01:01 +0900

    Fix video tag to be removed from section block.

--- src/Kunoichi/BlockLibrary/Blocks/Section.php
+++ src/Kunoichi/BlockLibrary/Blocks/Section.php
@@ -9,6 +9,13 @@ class Section extends BlockLibraryBase {
 
 	protected $block_name = 'section';
 
+	protected function init() {
+		add_filter( 'wp_kses_allowed_html', [ $this, 'allowed_html' ], 10, 2 );
+		add_filter( 'safe_style_css', [ $this, 'safe_styles' ] );
+		parent::init();
+	}
+
+
 	protected function localize_script() {
 		return apply_filters( 'kbl_section_container_class', [
 			'container_class'    => 'kbl-section-container',
@@ -30,4 +37,33 @@ class Section extends BlockLibraryBase {
 		return $content;
 	}
 
+	/**
+	 *
+	 *
+	 * @param array  $tags
+	 * @param string $context
+	 * @return array
+	 */
+	public function allowed_html( $tags, $context = '' ) {
+		if ( ! isset( $tags['source'] ) ) {
+			$tags['source'] = [
+				'src'   => true,
+				'type'  => true,
+				'media' => true,
+				'class' => true,
+			];
+		}
+		return $tags;
+	}
+
+	/**
+	 * Add allowed style tags.
+	 *
+	 * @param string[] $attrs
+	 * @return string[]
+	 */
+	public function safe_styles( $attrs ) {
+		$attrs[] = 'opacity';
+		return $attrs;
+	}
 }
